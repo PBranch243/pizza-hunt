fc58936497cd18ae89ad699f68aff860d56ab3ab
PhilB <philbranch243@gmail.com>
2022-08-30 10:42:22 -0500

    18.2.6

--- models/pizza.js
+++ models/pizza.js
@@ -31,13 +31,15 @@ const PizzaSchema = new Schema({
     }
 );
 
+//create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function () {
     return this.comments.length;
 });
 
-//create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+
 
 // export the pizza model
 module.exports = { Pizza };
